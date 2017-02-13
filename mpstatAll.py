c1c3ad4774b26e2ceaea48fc8ef526b015bd192d
Miguel Velez <miguelvelez@mijecu25.com>
2017-02-13 12:38:13 -0500

    Formatted output

--- mpstatAll.py
+++ mpstatAll.py
@@ -5,6 +5,15 @@ CPU_MONITOR_COMMAND = "mpstat -P ALL 1 30 | grep -E 'Average' | awk '{print $2, 
 
 output = open("cpu_tuple_output_" + strftime("%m%d") + '_' + strftime("%H%M%S") + ".txt","w",0)
 value = commands.getstatusoutput(CPU_MONITOR_COMMAND)[1]
-current_time= strftime("%H:%M:%S")
-output.write("time: {} | value: {}\n".format(current_time, float(value)))
 
+cpu_utilizations = value.strip().split('\n')[1:]
+
+all_stat = cpu_utilizations[0]
+cpu_utilizations.remove(all_stat)
+cpu_utilizations.append(all_stat)
+
+stats_instance = ''
+for cpu in cpu_utilizations:
+    stats_instance += cpu.strip().split(' ')[-1] + ","
+
+output.write(stats_instance[:-1])
