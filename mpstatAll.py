b243ffe093b0592909dd51e547e563575b98335b
Miguel Velez <miguelvelez@mijecu25.com>
2017-02-13 10:34:06 -0500

    Added script to get tuple of CPU utilization

---
+++ mpstatAll.py
@@ -0,0 +1,9 @@
+import commands
+from time import strftime
+
+CPU_MONITOR_COMMAND = "mpstat -P ALL 1 30 | grep -E 'Average' | awk '{print $2, $3}'"
+
+output = open("cpu_tuple_output_" + strftime("%m%d") + '_' + strftime("%H%M%S") + ".txt","w",0)
+value = commands.getstatusoutput(CPU_MONITOR_COMMAND)[1]
+output.write("time: {} | value: {}\n".format(current_time, float(value)))
+
